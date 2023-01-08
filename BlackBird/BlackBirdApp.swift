280e46673c623ab7ca311a5cd5e034207f959323
lcmichael926 <55223985+lcmichael926@users.noreply.github.com>
2023-01-08 15:02:27 +0800

    Initial Commit

---
+++ BlackBird/BlackBirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackBirdApp.swift
+//  BlackBird
+//
+//  Created by Michael Lau on 8/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct BlackBirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlackBird/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BlackBird
+//
+//  Created by Michael Lau on 8/1/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
